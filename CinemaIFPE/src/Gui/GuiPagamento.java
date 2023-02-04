121efd1e5eea148504b9c6358133b3b825a6c3ca
KácioSilva <101821745+KacioSilva@users.noreply.github.com>
2023-02-04 19:52:30 -0300

    update

--- CinemaIFPE/src/Gui/GuiPagamento.java
+++ CinemaIFPE/src/Gui/GuiPagamento.java
@@ -10,15 +10,30 @@ import Core.ControlePoltrona;
 import Core.Lanche;
 import Database.UpdatePoltronas;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.sql.SQLException;
 import java.awt.event.ActionEvent;
 import java.awt.Font;
+import javax.swing.JLabel;
+import java.awt.Color;
+import javax.swing.border.LineBorder;
+import javax.swing.JTextField;
 
 public class GuiPagamento extends JFrame {
 
     private JPanel contentPane;
+    private JButton btnDebito;
+    private JButton btnCredito;
+    private JButton btnPix;
+    private JButton pagar;
+    private JTextField txtEmail;
+    
+    private JLabel lblEmail;
+    private JTextField textField;
+    private JLabel lblCpf;
+  
 
 
     public static void main(String[] args) {
@@ -47,7 +62,8 @@ public class GuiPagamento extends JFrame {
         setContentPane(contentPane);
         contentPane.setLayout(null);
 
-        JButton pagar = new JButton("PAGAR");
+        pagar = new JButton("PAGAR");
+        pagar.setVisible(false);
         pagar.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
             	UpdatePoltronas upPoltrona = new UpdatePoltronas();
@@ -144,11 +160,11 @@ public class GuiPagamento extends JFrame {
 
             }
         });
-        pagar.setBounds(423, 294, 129, 57);
+        pagar.setBounds(413, 613, 89, 35);
         contentPane.add(pagar);
 
-        JButton voltar = new JButton("Voltar");
-        voltar.addActionListener(new ActionListener() {
+        JButton btnVoltar = new JButton("Voltar");
+        btnVoltar.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
             	
                 TelaLanche lanche = new TelaLanche();
@@ -156,8 +172,132 @@ public class GuiPagamento extends JFrame {
                 dispose();
             }
         });
-        voltar.setFont(new Font("Sitka Heading", Font.PLAIN, 15));
-        voltar.setBounds(10, 25, 75, 35);
-        contentPane.add(voltar);
+        btnVoltar.setFont(new Font("Sitka Heading", Font.PLAIN, 15));
+        btnVoltar.setBounds(10, 25, 75, 35);
+        contentPane.add(btnVoltar);
+        
+        JLabel cineifPaulista = new JLabel("CineIF Paulista");
+        cineifPaulista.setForeground(new Color(63, 164, 13, 236));
+        cineifPaulista.setFont(new Font("Sitka Heading", Font.BOLD | Font.ITALIC, 62));
+        cineifPaulista.setBounds(264, 11, 445, 132);
+        contentPane.add(cineifPaulista);
+        
+        JLabel lblNewLabel = new JLabel("PAGAMENTO");
+        lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 23));
+        lblNewLabel.setBounds(382, 154, 166, 35);
+        contentPane.add(lblNewLabel);
+        
+        JLabel lblSelecione = new JLabel("SELECIONE A FORMA");
+        lblSelecione.setFont(new Font("Tahoma", Font.PLAIN, 17));
+        lblSelecione.setBounds(21, 236, 172, 44);
+        contentPane.add(lblSelecione);
+        
+        btnDebito = new JButton("Débito");
+        btnDebito.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		
+        	}
+        });
+        btnDebito.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        btnDebito.setBounds(49, 402, 89, 23);
+        contentPane.add(btnDebito);
+        
+        btnCredito = new JButton("Crédito");
+        btnCredito.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        btnCredito.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		
+        	}
+        });
+        btnCredito.setBounds(49, 458, 89, 23);
+        contentPane.add(btnCredito);
+        
+        btnPix = new JButton("Pix");
+        btnPix.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        btnPix.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        	}
+        });
+        btnPix.setBounds(49, 555, 89, 23);
+        contentPane.add(btnPix);
+        
+        JLabel lblNewLabel_2 = new JLabel("Cartão:");
+        lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 15));
+        lblNewLabel_2.setBounds(63, 366, 87, 25);
+        contentPane.add(lblNewLabel_2);
+        
+        JLabel lblNewLabel_2_1 = new JLabel("Outra Forma:");
+        lblNewLabel_2_1.setFont(new Font("Tahoma", Font.BOLD, 15));
+        lblNewLabel_2_1.setBounds(45, 518, 112, 25);
+        contentPane.add(lblNewLabel_2_1);
+        
+        JButton btnHelp = new JButton("Help");
+        btnHelp.setFont(new Font("Sitka Heading", Font.PLAIN, 15));
+        btnHelp.setBounds(870, 29, 75, 35);
+        contentPane.add(btnHelp);
+        
+        JPanel panel_1 = new JPanel();
+        panel_1.setLayout(null);
+        panel_1.setBorder(new LineBorder(new Color(0, 0, 0)));
+        panel_1.setBounds(749, 308, 212, 189);
+        contentPane.add(panel_1);
+        
+        JLabel lblTotal = new JLabel("TOTAL GERAL:          R$0");
+        lblTotal.setBounds(10, 153, 199, 25);
+        panel_1.add(lblTotal);
+        lblTotal.setFont(new Font("Arial", Font.BOLD, 12));
+        
+        JLabel lblLanche = new JLabel("TOTAL LANCHES:     R$ 0");
+        lblLanche.setBounds(10, 84, 199, 25);
+        panel_1.add(lblLanche);
+        lblLanche.setFont(new Font("Arial", Font.BOLD, 12));
+        
+        JLabel lblIngresso = new JLabel();
+        lblIngresso.setBounds(10, 11, 308, 25);
+        panel_1.add(lblIngresso);
+        lblIngresso.setText("TOTAL INGRESSO:    R$0");
+        lblIngresso.setFont(new Font("Arial", Font.BOLD, 12));
+        
+        JLabel lblNewLabel_3 = new JLabel("Custos");
+        lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 17));
+        lblNewLabel_3.setBounds(820, 257, 112, 33);
+        contentPane.add(lblNewLabel_3);
+        
+        JLabel lblDe = new JLabel("DE");
+        lblDe.setFont(new Font("Tahoma", Font.PLAIN, 17));
+        lblDe.setBounds(80, 269, 36, 44);
+        contentPane.add(lblDe);
+        
+        JLabel lblPagamento = new JLabel("PAGAMENTO");
+        lblPagamento.setFont(new Font("Tahoma", Font.PLAIN, 17));
+        lblPagamento.setBounds(45, 305, 112, 44);
+        contentPane.add(lblPagamento);
+        
+        txtEmail = new JTextField();
+        txtEmail.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        txtEmail.setBounds(392, 305, 156, 20);
+        contentPane.add(txtEmail);
+        txtEmail.setColumns(10);
+        
+        lblEmail = new JLabel("Email:");
+        lblEmail.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        lblEmail.setBounds(348, 306, 43, 18);
+        contentPane.add(lblEmail);
+        
+        textField = new JTextField();
+        textField.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        textField.setColumns(10);
+        textField.setBounds(392, 352, 156, 20);
+        contentPane.add(textField);
+        
+        lblCpf = new JLabel("CPF:");
+        lblCpf.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        lblCpf.setBounds(348, 353, 30, 18);
+        contentPane.add(lblCpf);
+        
+//        ImageIcon logo_ifpe = new ImageIcon((getClass().getResource("/midia/ifpe.png")));
+//        JLabel ifpe = new JLabel(logo_ifpe);
+//        getContentPane().add(ifpe);
+//        ifpe.setBounds(327,236,284,281);
     }
 }
